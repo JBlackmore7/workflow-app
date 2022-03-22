fa5573daea9a922ef0456424d7a861936163e2df
Jared Blackmore <jblackmore17@gmail.com>
2022-03-22 08:23:04 -0230

    Add files via upload

---
+++ workflow-app/forms/update.js
@@ -0,0 +1,8 @@
+function logSubmit(event) {
+  log.textContent = `Work Order Updated!`;
+  event.preventDefault();
+}
+
+const form = document.getElementById("adminInput");
+const log = document.getElementById("log");
+form.addEventListener("submit", logSubmit);
