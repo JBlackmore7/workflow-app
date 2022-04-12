4a4d715ee2eebad6aa14ec6afd9a680828cf220e
Jared Blackmore <jblackmore17@gmail.com>
2022-04-12 17:52:36 -0230

    Delete workflow-app directory

--- workflow-app/forms/update.js
+++
@@ -1,8 +0,0 @@
-function logSubmit(event) {
-  log.textContent = `Work Order Updated!`;
-  event.preventDefault();
-}
-
-const form = document.getElementById("adminInput");
-const log = document.getElementById("log");
-form.addEventListener("submit", logSubmit);
