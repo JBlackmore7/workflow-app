429fc2dd3e4556eee2cbc077c126ba250be1364f
Jared Blackmore <jblackmore17@gmail.com>
2022-04-12 17:50:26 -0230

    Delete forms directory

--- forms/submit.js
+++
@@ -1,8 +0,0 @@
-function logSubmit(event) {
-  log.textContent = `Work Order Created!`;
-  event.preventDefault();
-}
-
-const form = document.getElementById("createForm");
-const log = document.getElementById("log");
-form.addEventListener("submit", logSubmit);
