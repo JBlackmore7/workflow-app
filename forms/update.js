6b4b35ba72516cb81c52a43f7c590cedf84a74f5
Jared Blackmore <jblackmore17@gmail.com>
2022-04-30 21:33:58 -0230

    Add files via upload

--- forms/update.js
+++ forms/update.js
@@ -1,22 +1,10 @@
-/*function logSubmit(event) {
-  log.textContent = `Work Order Updated!`;
-  event.preventDefault();
-}
+let createOrder;
 
-const form = document.getElementById("adminInput");
-const log = document.getElementById("log");
-form.addEventListener("submit", logSubmit);*/
-
-//Get the form element by id
-
-let createOrder = null;
+const urlSearchParams = new URLSearchParams(window.location.search);
+const workorder_id = urlSearchParams.get("workorder_id");
 
 window.addEventListener("DOMContentLoaded", (event) => {
   console.log("DOM fully loaded and parsed");
-
-  const urlSearchParams = new URLSearchParams(window.location.search);
-  const workorder_id = urlSearchParams.get("workorder_id");
-  console.log(workorder_id);
 
   fetch("http://localhost:3000/createOrder/" + workorder_id)
     .then((response) => response.json())
@@ -28,3 +16,56 @@ window.addEventListener("DOMContentLoaded", (event) => {
       console.error("Error:", error);
     });
 });
+
+/*
+let formData = new FormData(serviceWork);
+const formElem = document.querySelector("form");
+
+formElem.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  fetch("http://localhost:3000/createOrder/" + workorder_id, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(Object.fromEntries(formData.entries())),
+  });
+});
+*/
+
+function submitForm(event) {
+  // Prevent the form from submitting.
+  event.preventDefault();
+  // Set url for submission and collect data.
+  const url = "http://localhost:3000/createOrder/" + workorder_id;
+  const formData = new FormData(event.target);
+  // Build the data object.
+  const data = {};
+  formData.forEach((value, key) => (data[key] = value));
+  // Log the data.
+  console.log(data);
+}
+
+function addRows() {
+  var table = document.getElementById("materials");
+  var rowCount = table.rows.length;
+  var cellCount = table.rows[0].cells.length;
+  var row = table.insertRow(rowCount);
+  for (var i = 0; i < cellCount; i++) {
+    var cell = "cell" + i;
+    cell = row.insertCell(i);
+    var copyCell = document.getElementById("col" + i).innerHTML;
+    cell.innerHTML = copyCell;
+  }
+}
+function deleteRows() {
+  var table = document.getElementById("materials");
+  var rowCount = table.rows.length;
+  if (rowCount > "2") {
+    var row = table.deleteRow(rowCount - 1);
+    rowCount--;
+  } else {
+    alert("There should be at least one row");
+  }
+}
