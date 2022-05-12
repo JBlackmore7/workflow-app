4148ad175ce389a5ede4e35a114feb60500a3f5e
Jared Blackmore <jblackmore17@gmail.com>
2022-05-11 22:30:45 -0230

    Add files via upload

---
+++ forms/submitServiceForm.js
@@ -0,0 +1,86 @@
+let createOrder;
+
+window.addEventListener("DOMContentLoaded", (event) => {
+  console.log("DOM fully loaded and parsed");
+
+  const urlSearchParams = new URLSearchParams(window.location.search);
+  const workorder_id = urlSearchParams.get("workorder_id");
+
+  fetch("http://localhost:3000/createOrder/" + workorder_id)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log("Success:", data);
+      createOrder = data;
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
+
+  const form = document.querySelector("form");
+  form.addEventListener("submit", handleSubmit);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    const serviceData = new FormData(e.target);
+
+    let object = {};
+    object.serviceDetails = serviceData.get("serviceDetails");
+    object.truckStock = serviceData.get("truckStock");
+    object.condemnationDetails = serviceData.get("condemnationDetails");
+
+    let q = serviceData.getAll("quantity");
+    let pn = serviceData.getAll("partNumber");
+    let pd = serviceData.getAll("partDescription");
+
+    let arr = [];
+    for (var i = 0; i < q.length; i++) {
+      arr[i] = { quantity: q[i], partNumber: pn[i], partDescription: pd[i] };
+    }
+    object.parts = arr;
+    object["parts"] = arr;
+
+    createOrder.service = object;
+    let json = JSON.stringify(createOrder);
+    console.log(json);
+
+    fetch("http://localhost:3000/createOrder/" + workorder_id, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: json,
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("Success:", data);
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  }
+});
+
+function addRows() {
+  var table = document.getElementById("materials");
+  var rowCount = table.rows.length;
+  var cellCount = table.rows[0].cells.length;
+  var row = table.insertRow(rowCount);
+  for (var i = 0; i < cellCount; i++) {
+    var cell = "cell" + i;
+    cell = row.insertCell(i);
+
+    var copyCell = document.getElementById("col" + i).innerHTML;
+    cell.innerHTML = copyCell;
+  }
+}
+function deleteRows() {
+  var table = document.getElementById("materials");
+  var rowCount = table.rows.length;
+  if (rowCount > "2") {
+    var row = table.deleteRow(rowCount - 1);
+    rowCount--;
+  } else {
+    alert("There should be at least one row");
+  }
+}
