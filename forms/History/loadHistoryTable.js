5022e33044fbc1a50ec0819bf8fca104053babb7
Jared Blackmore <jblackmore17@gmail.com>
2022-05-29 00:00:26 -0230

    Add files via upload

--- forms/History/loadHistoryTable.js
+++ forms/History/loadHistoryTable.js
@@ -33,8 +33,8 @@ async function loadIntoTable(url, table) {
     let history = document.createElement("button");
     history.innerHTML = "View";
     history.setAttribute("workorder_id", row.id);
-    history.onclick = showModal;
-    let h1 = document.getElementById("modal-header");
+    history.setAttribute("data-modal-target", "#modal");
+    let h1 = document.getElementById("modal-title");
     h1.innerHTML = "Work Order #" + row.id;
     Object.keys(headers).forEach((element) => {
       const cellElement = document.createElement("td");
@@ -46,15 +46,58 @@ async function loadIntoTable(url, table) {
     tableBody.appendChild(rowElement);
   }
 
-  // Modal Configuring
-  function showModal() {
-    document.getElementById("modal").style.display = "block";
-    document.getElementById("companyName").innerHTML =
-      "Company Name: " + localWorkorderRef.find((e) => e.name === "companyName");
+  const openModalButtons = document.querySelectorAll("[data-modal-target]");
+  const closeModalButtons = document.querySelectorAll("[data-close-button]");
+  const overlay = document.getElementById("overlay");
+
+  openModalButtons.forEach((button) => {
+    button.addEventListener("click", () => {
+      const modal = document.querySelector(button.dataset.modalTarget);
+      openModal(modal);
+    });
+  });
+
+  overlay.addEventListener("click", () => {
+    const modals = document.querySelectorAll(".modal.active");
+    modals.forEach((modal) => {
+      closeModal(modal);
+    });
+  });
+
+  closeModalButtons.forEach((button) => {
+    button.addEventListener("click", () => {
+      const modal = button.closest(".modal");
+      closeModal(modal);
+    });
+  });
+
+  function openModal(modal) {
+    if (modal == null) return;
+    const workorderID = e.currentTarget.getAttribute("workorder_id");
+    const workorderRef = localWorkorderRef.find((e) => e.id == workorderID);
+    const myHelpfulFunction = (key, value) => (document.getElementById(key).innerHTML += " " + value);
+    myHelpfulFunction("companyName", workorderRef.companyName);
+    myHelpfulFunction("companyAddress", workorderRef.companyAddress);
+    myHelpfulFunction("workSite", workorderRef.worksite);
+    myHelpfulFunction("contactName", workorderRef.contactName);
+    myHelpfulFunction("contactNumber", workorderRef.contactNumber);
+    myHelpfulFunction("contactEmail", workorderRef.contactEmail);
+    myHelpfulFunction("orderDetails", workorderRef.details);
+    myHelpfulFunction("date", workorderRef.date);
+    myHelpfulFunction("createdBy", workorderRef.employeeName);
+    myHelpfulFunction("serviceDetails", workorderRef.service.serviceDetails);
+    myHelpfulFunction("jobNumber", workorderRef.jobNumber);
+    myHelpfulFunction("notes", workorderRef.service.condemnationDetails);
+    myHelpfulFunction("truckStock", workorderRef.service.truckStock);
+    modal.classList.add("active");
+    overlay.classList.add("active");
   }
 
-  let closeButton = document.getElementById("closeModal");
-  closeButton.innerHTML = "&#10006";
+  function closeModal(modal) {
+    if (modal == null) return;
+    modal.classList.remove("active");
+    overlay.classList.remove("active");
+  }
 }
 
 loadIntoTable("http://localhost:3000/completeOrder", document.querySelector("table"));
