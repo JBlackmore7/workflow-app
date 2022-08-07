dc0bd0814c671b8598a46771fd1e03e432be67b5
Jared Blackmore <jblackmore17@gmail.com>
2022-08-07 20:54:18 -0230

    navbar converted to injectable JS

--- main.js
+++ main.js
@@ -1,3 +1,50 @@
+const nav = `<header class="navHeader">
+      <a class="navTitle" href="/index.html">Work Flow</a>
+      <nav>
+        <div class="dropdown" hidden>
+          <img src="/images/navbar-1.1s-200px.png" class="dropbtn" onclick="dropdown()" />
+          <div class="dropdown-content" id="myDropdown">
+            <a class="navAnchor" href="/forms/Create/createForm.html"
+              >Create Order<span class="navPic"><img src="/images/create.png" /></span
+            ></a>
+            <a class="navAnchor" href="/forms/Admin/admin.html"
+              >Admin Input<span class="navPic"><img src="/images/admin.png" /></span
+            ></a>
+            <a class="navAnchor" href="/forms/Service/service.html"
+              >Service<span class="navPic"><img src="/images/service.png" /></span
+            ></a>
+            <a class="navAnchor" href="/forms/History/history.html"
+              >History<span class="navPic"><img src="/images/history.png" /></span
+            ></a>
+          </div>
+        </div>
+        <ul class="navLinks">
+          <li class="navList">
+            <a class="navAnchor" href="/forms/History/history.html"
+              >History<span class="navPic"><img src="/images/history.png" /></span
+            ></a>
+          </li>
+          <li class="navList">
+            <a class="navAnchor" href="/forms/Service/service.html"
+              >Service<span class="navPic"><img src="/images/service.png" /></span
+            ></a>
+          </li>
+          <li class="navList">
+            <a class="navAnchor" href="/forms/Admin/admin.html"
+              >Admin Input<span class="navPic"><img src="/images/admin.png" /></span
+            ></a>
+          </li>
+          <li class="navList">
+            <a class="navAnchor" href="/forms/Create/createForm.html"
+              >Create Order<span class="navPic"><img src="/images/create.png" /></span
+            ></a>
+          </li>
+        </ul>
+      </nav>
+    </header>`;
+
+document.body.insertAdjacentHTML("afterbegin", nav);
+
 const overlay = document.getElementById("overlay");
 
 function openModal() {
